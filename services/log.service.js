8e6f55b28a22030b1706a30e9af2023e09dcb20f
emin1731 <emin1731@icloud.com>
2024-06-11 12:59:49 +0400

    Add services for logging messages and saving token in system

---
+++ services/log.service.js
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen("SUCCESS:") + " " + message);
+};
+
+const printError = (message) => {
+    console.log(chalk.bgRed("ERROR:") + " " + message);
+};
+
+const printHelp = (message) => {
+    console.log(dedent`
+    ${chalk.bgCyan("HELP: ")}
+    No params - get weather 
+    -s [city] - set city 
+    -h - help menu 
+    -t [api_key] - set token
+
+` );
+};
+
+
+export {printError, printSuccess, printHelp};
--- weather.js
+++ weather.js
@@ -1,19 +1,31 @@
 #!user/bin/env node 
 import { getArgs } from "./helpers/args.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js"
 
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue("token", token);
+        printSuccess("Token is saved");
+    } catch (error) {
+        printError(error.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
+    // console.log(args);
 
     if (args.h) {
-        // Return help
+        printHelp();
     }
     if (args.s) {
         // Return s
     }
     if (args.t) {
         // Return t
+        saveToken(args.t);
+        
     }
     // Get weather 
 };
