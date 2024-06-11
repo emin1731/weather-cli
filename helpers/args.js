117261ef1c993e2333d6de8a270cf3bf03857025
emin1731 <emin1731@icloud.com>
2024-06-11 20:48:47 +0400

    format code

--- helpers/args.js
+++ helpers/args.js
@@ -1,20 +1,17 @@
-
 const getArgs = (args) => {
-    const res = {};
-    const [executor, filename, ...rest] = args;
-    rest.forEach((item, index, array) => {
-        if (item.charAt(0) == "-") {
-            if (index == array.length - 1) {
-                res[item.substring(1)] = true;
-            }
-            else if (array[index + 1].charAt(0) != "-") {
-                res[item.substring(1)] = array[index + 1];
-            }
-            else {
-                res[item.substring(1)] = true;
-            }
-        }
-    });
-    return res;
-}
+  const res = {};
+  const [executor, filename, ...rest] = args;
+  rest.forEach((item, index, array) => {
+    if (item.charAt(0) == "-") {
+      if (index == array.length - 1) {
+        res[item.substring(1)] = true;
+      } else if (array[index + 1].charAt(0) != "-") {
+        res[item.substring(1)] = array[index + 1];
+      } else {
+        res[item.substring(1)] = true;
+      }
+    }
+  });
+  return res;
+};
 export { getArgs };
